d9dd71e84c0be9cbe58f0a974096edb054c2a17a
sonatine726 <sonatine726@gmail.com>
2018-02-18 11:00:17 +0900

    Deal with tel link only for smartphone.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 $(function() {
-     $('.p-top-img-sld').slick({
-          infinite: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-          fade: true,
-          asNavFor: '.p-top-img-sld-thumb'
-     });
-     $('.p-top-img-sld-thumb').slick({
-          infinite: true,
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          asNavFor: '.p-top-img-sld',
-          focusOnSelect: true,
-     });
+  $('.p-top-img-sld').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.p-top-img-sld-thumb'
+  });
+  $('.p-top-img-sld-thumb').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.p-top-img-sld',
+    focusOnSelect: true,
+  });
+  if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
+    $('.u-tel-number').each(function() {
+      var str = $(this).html();
+      $(this).html($('<a>').attr('href', 'tel:' + $(this).text().replace(/-/g, '')).append(str + '</a>'));
+    });
+  }
 });
 
 $(window).on('load',function(){
