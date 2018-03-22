12f7bc6b87664b33a3c66cbd7ab2b1e4f5e833c1
sonatine726 <sonatine726@gmail.com>
2018-03-22 15:14:34 +0900

    Commit intermediate.(Making millet page.)

---
+++ js/cropfields/rf_hoshinojo_3.js
@@ -0,0 +1,54 @@
+if (typeof riceFields === "undefined") {
+  var riceFields = [];
+}
+
+var hoshinojo_3_dict = {fieldId:"hoshinojo_3", 
+                        fieldName:"星3", 
+                        polCenter:{lat: 35.68152469371044, lng: 137.91770539542475},
+                        polArray:[{lat: 35.68180683299479, lng: 137.91715899113115},
+                                  {lat: 35.68181119034394, lng: 137.9174754917949},
+                                  {lat: 35.68165323628485, lng: 137.91789793971475},
+                                  {lat: 35.68158787589309, lng: 137.9178912341922},
+                                  {lat: 35.6815192474241, lng: 137.9181393385261},
+                                  {lat: 35.68117610419377, lng: 137.91815274957116},
+                                  {lat: 35.681118368838554, lng: 137.917222023043}
+                        ],
+                        url:"ricefields/rf_hoshinojo_3.html",
+                        zoom: 18,
+                        block: "星野城",
+                        number: 3,
+                        address: "2834",
+                        area: 47};
+
+riceFields.push(hoshinojo_3_dict);
+
+$(function(){
+  if($('ul.p-ricefield-list').length){
+    let appendLi = $("<li>").attr("class", "p-rcf-list-item").append(
+                            $("<a>").attr("href", hoshinojo_3_dict.url).text(hoshinojo_3_dict.fieldName));
+    $('ul.p-ricefield-list').append(appendLi);
+    hoshinojo_3_dict["linkLiDom"] = appendLi;
+  }
+
+  if($("td.p-rfc-prop-block")[0]){
+    $("td.p-rfc-prop-block").text(hoshinojo_3_dict.block);
+  }
+  if($("td.p-rfc-prop-number")[0]){
+    $("td.p-rfc-prop-number").text(hoshinojo_3_dict.number);
+  };
+  if($("td.p-rfc-prop-fieldName")[0]){
+    $("td.p-rfc-prop-fieldName").text(hoshinojo_3_dict.fieldName);
+  }
+  if($("td.p-rfc-prop-address")[0]){
+    $("td.p-rfc-prop-address").text(hoshinojo_3_dict.address);
+  }
+  if($("td.p-rfc-prop-area")[0]){
+    $("td.p-rfc-prop-area").text(hoshinojo_3_dict.area + "a");
+  }
+
+  if($('ol.p-ricefield-individual_page')[0]){
+    const appendLi = $("<li>").append(
+                            $("<a>").attr("href", hoshinojo_3_dict.url).text("圃場 「" + hoshinojo_3_dict.fieldName + "」"));
+    $('ol.p-ricefield-individual_page').append(appendLi);
+  }
+});
---
+++ js/cropfields/rf_kou_2.js
@@ -0,0 +1,52 @@
+if (typeof riceFields === "undefined") {
+  var riceFields = [];
+}
+
+const kou_2_dict = {fieldId:"kou_2", 
+                    fieldName:"好2", 
+                    polCenter:{lat: 35.68557388966852, lng: 137.92927372534177},
+                    polArray:[{lat: 35.685878890059556, lng: 137.9290977724304},
+                              {lat: 35.68590503281662, lng: 137.9292587049713},
+                              {lat: 35.685878890059556, lng: 137.9294410951843},
+                              {lat: 35.68510331770344, lng: 137.92945182402036},
+                              {lat: 35.68510331770344, lng: 137.92911923010251}
+                    ],
+                    url:"ricefields/rf_kou_2.html",
+                    zoom: 18,
+                    block: "座光寺域",
+                    number: 2,
+                    address: "2095-6",
+                    area: 28};
+
+riceFields.push(kou_2_dict);
+
+$(function(){
+  if($('ul.p-ricefield-list').length){
+    let appendLi = $("<li>").attr("class", "p-rcf-list-item").append(
+                            $("<a>").attr("href", kou_2_dict.url).text(kou_2_dict.fieldName));
+    $('ul.p-ricefield-list').append(appendLi);
+    kou_2_dict["linkLiDom"] = appendLi;
+  }
+
+  if($("td.p-rfc-prop-block")[0]){
+    $("td.p-rfc-prop-block").text(kou_2_dict.block);
+  }
+  if($("td.p-rfc-prop-number")[0]){
+    $("td.p-rfc-prop-number").text(kou_2_dict.number);
+  };
+  if($("td.p-rfc-prop-fieldName")[0]){
+    $("td.p-rfc-prop-fieldName").text(kou_2_dict.fieldName);
+  }
+  if($("td.p-rfc-prop-address")[0]){
+    $("td.p-rfc-prop-address").text(kou_2_dict.address);
+  }
+  if($("td.p-rfc-prop-area")[0]){
+    $("td.p-rfc-prop-area").text(kou_2_dict.area + "a");
+  }
+
+  if($('ol.p-ricefield-individual_page')[0]){
+    const appendLi = $("<li>").append(
+                            $("<a>").attr("href", kou_2_dict.url).text("圃場 「" + kou_2_dict.fieldName + "」"));
+    $('ol.p-ricefield-individual_page').append(appendLi);
+  }
+});
