c24781c6ca94c4d5d4947cb9e8a5fad31c56e640
sonatine726 <sonatine726@gmail.com>
2018-03-30 21:33:40 +0900

    Extract common parts about header and footer and load these by js.

--- inquiry/MailForm01_utf8/header.php
+++ inquiry/MailForm01_utf8/header.php
@@ -13,8 +13,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="../../css/style.css">
-  <link rel="stylesheet" type="text/css" href="css/inquiry.css">
+  <link rel="stylesheet" type="text/css" href="/css/style.css">
+  <link rel="stylesheet" type="text/css" href="../css/inquiry.css">
   <title>お米の座光寺農園　お問い合わせ</title>
   <meta content="お米の座光寺農園 お問い合わせ" name="title">
   <meta content="長野県飯島町で真摯にお米作りに打ち込んでいます、お米の座光寺農園です。毎日口に入れるお米、安心や美味しさを心から実感できるものを提供したいと考えています。そのために、お米の生育環境、農法、そして生産者自身の情報を、積極的に提供させていただきます。どこで作っているのか、どんな風につくっているのか、誰が作っているのかなどを皆様に知っていただき、そこに共感を得ていただきましたら、ぜひ座光寺農園のお米を食してみてください。" name="description">
@@ -29,11 +29,11 @@
   <meta property="fb:app_id" content="353304885151501" />
   <meta content="summary_large_image" name="twitter:card" />
   <meta content="@zakojifarm" name="twitter:site" />
-  <link rel="icon" type="image/x-icon" href="../../images/favicon.ico">
-  <link rel="icon" type="image/png" href="../../images/favicon-16x16.png" sizes="16x16">
-  <link rel="icon" type="image/png" href="../../images/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="../../images/favicon-96x96.png" sizes="96x96">
-  <link rel="apple-touch-icon" href="../../images/apple-touch-icon.png" />
+  <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+  <link rel="icon" type="image/png" href="/images/favicon-16x16.png" sizes="16x16">
+  <link rel="icon" type="image/png" href="/images/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/images/favicon-96x96.png" sizes="96x96">
+  <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
   <link rel="canonical" href="https://zakojifarm.jp/inquiry/inquiry.html">
   <script type="application/ld+json">
   {
@@ -64,44 +64,9 @@
   </script>
 </head>
 <body>
-  <header id="header">
-    <section class="c-title-and-sns">
-      <section class="c-logo">
-        <a href="../../index.html"><img src="../../images/ZakoniNoen_Icon.png" alt="座光寺農園" class="c-logo__img"></a>
-      </section>
-      <section class="p-smallmap">
-        <img src="../../images/SmallMap.gif" alt="座光寺農園の位置" class="p-smallmap__img">
-        <section class="p-iijima-string">
-          長野県飯島町
-        </section>
-      </section>
-      <section class="p-sns-icons">
-        <div class="p-sns-icons__box">
-          <section class="p-sns-icon__list">
-            <ul class="p-sns-icon__ul">
-              <li class="p-sns-icon"><a href="https://www.facebook.com/zakojifarm/"><img src="../../images/FB_Icon.png" alt="座光寺農園Facebook" class="p-sns-icon__img"></a></li>
-              <li class="p-sns-icon"><a href="https://www.instagram.com/zakojifarm/"><img src="../../images/Insta_Icon.png" alt="座光寺農園Instagram" class="p-sns-icon__img"></a></li>
-              <li class="p-sns-icon"><a href="https://twitter.com/zakojifarm"><img src="../../images/Twit_Icon.png" alt="座光寺農園Twittter" class="p-sns-icon__img"></a></li>
-              <li class="p-sns-icon"><a href="https://www.youtube.com/channel/UCfeU1UPUn7iktwZpWOkefFA"><img src="../../images/YT_Icon.png" alt="座光寺農園YouTubeチャンネル" class="p-sns-icon__img"></a></li>
-            </ul>
-          </section>
-        </div>
-      </section>
-    </section>
-    <nav class="c-gnav">
-      <ul class="c-gnav-box">
-        <li class="c-gnav-item"><a href="../../introduction/introduction.html">座光寺農園のご紹介</a></li>
-        <li class="c-gnav-item"><a href="../../rice/rice.html">お米</a></li>
-        <li class="c-gnav-item"><a href="../../millet/millet.html">雑穀</a></li>
-        <li class="c-gnav-item"><a href="../../satoyama/satoyama.html">里山の風景</a></li>
-        <li class="c-gnav-item"><a href="../../blogs" target="_blank">ブログ</a></li>
-        <li class="c-gnav-item"><a href="https://zakojifarm.shopselect.net/" target="_blank">ショップ</a></li>
-        <li class="c-gnav-item"><a href="../../inquiry/inquiry.html">お問合せ</a></li>
-      </ul>
-    </nav>
-    <ol class="c-breadcrumb">
-      <li><a href="../../index.html">ホーム</a></li>
-      <li><a href="rice.html">お問合せ</a></li>
-    <ol>
-  </header>
+  <header id="header"></header>
   <main id="main">
+    <ol class="c-breadcrumb">
+      <li><a href="/">ホーム</a></li>
+      <li><a href="inquiry.html">お問合せ</a></li>
+    </ol>
