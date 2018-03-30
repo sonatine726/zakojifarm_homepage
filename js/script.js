b514673aaed968a0f11c04447b4e71efda1d1be3
sonatine726 <sonatine726@gmail.com>
2018-03-30 21:33:40 +0900

    Extract common parts about header and footer and load these by js.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-$(function() {
-  if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
-    $('.u-tel-number').each(function() {
-      var str = $(this).html();
-      $(this).html($('<a>').attr('href', 'tel:' + $(this).text().replace(/-/g, '')).append(str + '</a>'));
-    });
-  }
-});
+(function($){
+  $(function() {
+    $("#header").load("/common/header.html");
+    $("#footer").load("/common/footer.html");
+
+    if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
+      $('.u-tel-number').each(function() {
+        var str = $(this).html();
+        $(this).html($('<a>').attr('href', 'tel:' + $(this).text().replace(/-/g, '')).append(str + '</a>'));
+      });
+    }
+  });
+})(jQuery);
