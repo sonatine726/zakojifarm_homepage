86f86321ea4eca08dfcb455a51c6e7e726f3703f
sonatine726 <sonatine726@gmail.com>
2018-03-31 16:02:21 +0900

    - Refactor functions for millet field. - Add "use strict" to js for millet.

--- rice/js/rice.js
+++ rice/js/rice.js
@@ -21,25 +21,3 @@ $(function(){
 $(window).on('unload',function(){
   GUnload();
 });
-
-// if (typeof riceFields === "undefined") {
-//   var riceFields = [];
-// }
-
-// $(window).on('load',function(){
-//   const mapTargetId = "p-cropfield-map_canvas";
-//   let centerPos;
-//   let zoom;
-//   if(riceFields.length == 1){
-//     centerPos = riceFields[0].polCenter;
-//     zoom = riceFields[0].zoom;
-//   }
-//   else{
-//     centerPos = {lat:35.68177567810094, lng:137.91623926175816};
-//     zoom = 17;
-//   }
-//   viewGMap(mapTargetId, centerPos, zoom, riceFields);
-// });
-// $(window).on('unload',function(){
-//   GUnload();
-// });
