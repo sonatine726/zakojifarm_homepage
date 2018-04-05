a09ac6e96734c3010e2a79d458396afec0315cbe
sonatine726 <sonatine726@gmail.com>
2018-04-06 08:40:18 +0900

    - Chage petal count and fall speed for smartphone. - Stop rotation of petals for smartphone.

--- js/cherryblossom.js
+++ js/cherryblossom.js
@@ -3,7 +3,7 @@
     (function(d,b,w){
       var q = d.createElement('div');
       q.id = "sakura";
-      q.innerHTML = '<style>'+
+      let htmlCode = '<style>'+
       'html,body{overflow-x:hidden;}'+
       '.hana{'+
       'position:absolute;height:0;width:0;'+
@@ -31,76 +31,81 @@
       '.t3::after{border-color:#ffd1d9;}'+
       '.t4::after{border-color:#ffc0cb;}'+
       '.t5::after{border-color:#ffafbd;}'+
-      '.t6::after{border-color:#ffafbd;}'+
+      '.t6::after{border-color:#ffafbd;}';
 
-      '.y1{-webkit-animation:v1 10s infinite;animation:v1 10s infinite;}'+
-      '.y2{-webkit-animation:v2 10s infinite;animation:v2 10s infinite;}'+
-      '.y3{-webkit-animation:v3 9s infinite;animation:v3 9s infinite;}'+
-      '.y4{-webkit-animation:v4 9s infinite;animation:v4 9s infinite;}'+
-      '.y5{-webkit-animation:v5 8s infinite;animation:v5 8s infinite;}'+
-      '.y6{-webkit-animation:v6 8s infinite;animation:v6 8s infinite;}'+
-      '@-webkit-keyframes v1{'+
-        'from{-webkit-transform: rotate(0deg) scale(1);}'+
-        '50%{-webkit-transform: rotate(270deg) scale(1);}'+
-        'to{-webkit-transform: rotate(1deg) scale(1);}'+
-      '}'+
-      '@-webkit-keyframes v2{'+
-        'from{-webkit-transform: rotate(-90deg) scale(.9);}'+
-        '50%{-webkit-transform: rotate(-360deg) scale(.9);}'+
-        'to{-webkit-transform: rotate(-89deg) scale(.9);}'+
-      '}'+
-      '@-webkit-keyframes v3{'+
-        'from{-webkit-transform: rotate(30deg) scale(.8);}'+
-        '50%{-webkit-transform: rotate(300deg) scale(.8);}'+
-        'to{-webkit-transform: rotate(29deg) scale(.8);}'+
-      '}'+
-      '@-webkit-keyframes v4{'+
-        'from{-webkit-transform: rotate(-120deg) scale(.7);}'+
-        '50%{-webkit-transform: rotate(-390deg) scale(.7);}'+
-        'to{-webkit-transform: rotate(-119deg) scale(.7);}'+
-      '}'+
-      '@-webkit-keyframes v5{'+
-        'from{-webkit-transform: rotate(60deg) scale(.6);}'+
-        '50%{-webkit-transform: rotate(330deg) scale(.6);}'+
-        'to{-webkit-transform: rotate(59deg) scale(.6);}'+
-      '}'+
-      '@-webkit-keyframes v6{'+
-        'from{-webkit-transform: rotate(-150deg) scale(.5);}'+
-        '50%{-webkit-transform: rotate(-420deg) scale(.5);}'+
-        'to{-webkit-transform: rotate(-149deg) scale(.5);}'+
-      '}'+
-      '@keyframes v1{'+
-        'from{transform: rotate(0deg) scale(1);}'+
-        '50%{transform: rotate(270deg) scale(1);}'+
-        'to{transform: rotate(1deg) scale(1);}'+
-      '}'+
-      '@keyframes v2{'+
-        'from{transform: rotate(-90deg) scale(.9);}'+
-        '50%{transform: rotate(-360deg) scale(.9);}'+
-        'to{transform: rotate(-89deg) scale(.9);}'+
-      '}'+
-      '@keyframes v3{'+
-        'from{transform: rotate(30deg) scale(.8);}'+
-        '50%{transform: rotate(300deg) scale(.8);}'+
-        'to{transform: rotate(29deg) scale(.8);}'+
-      '}'+
-      '@keyframes v4{'+
-        'from{transform: rotate(-120deg) scale(.7);}'+
-        '50%{transform: rotate(-390deg) scale(.7);}'+
-        'to{transform: rotate(-119deg) scale(.7);}'+
-      '}'+
-      '@keyframes v5{'+
-        'from{transform: rotate(60deg) scale(.6);}'+
-        '50%{transform: rotate(330deg) scale(.6);}'+
-        'to{transform: rotate(59deg) scale(.6);}'+
-      '}'+
-      '@keyframes v6{'+
-        'from{transform: rotate(-150deg) scale(.5);}'+
-        '50%{transform: rotate(-420deg) scale(.5);}'+
-        'to{transform: rotate(-149deg) scale(.5);}'+
-      '}'+
+      if (!((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0)) {
+        htmlCode = htmlCode +
+         '.y1{-webkit-animation:v1 10s infinite;animation:v1 10s infinite;}'+
+        '.y2{-webkit-animation:v2 10s infinite;animation:v2 10s infinite;}'+
+        '.y3{-webkit-animation:v3 9s infinite;animation:v3 9s infinite;}'+
+        '.y4{-webkit-animation:v4 9s infinite;animation:v4 9s infinite;}'+
+        '.y5{-webkit-animation:v5 8s infinite;animation:v5 8s infinite;}'+
+        '.y6{-webkit-animation:v6 8s infinite;animation:v6 8s infinite;}'+
+        '@-webkit-keyframes v1{'+
+          'from{-webkit-transform: rotate(0deg) scale(1);}'+
+          '50%{-webkit-transform: rotate(270deg) scale(1);}'+
+          'to{-webkit-transform: rotate(1deg) scale(1);}'+
+        '}'+
 
-      '</style>';
+        '@-webkit-keyframes v2{'+
+          'from{-webkit-transform: rotate(-90deg) scale(.9);}'+
+          '50%{-webkit-transform: rotate(-360deg) scale(.9);}'+
+          'to{-webkit-transform: rotate(-89deg) scale(.9);}'+
+        '}'+
+        '@-webkit-keyframes v3{'+
+          'from{-webkit-transform: rotate(30deg) scale(.8);}'+
+          '50%{-webkit-transform: rotate(300deg) scale(.8);}'+
+          'to{-webkit-transform: rotate(29deg) scale(.8);}'+
+        '}'+
+        '@-webkit-keyframes v4{'+
+          'from{-webkit-transform: rotate(-120deg) scale(.7);}'+
+          '50%{-webkit-transform: rotate(-390deg) scale(.7);}'+
+          'to{-webkit-transform: rotate(-119deg) scale(.7);}'+
+        '}'+
+        '@-webkit-keyframes v5{'+
+          'from{-webkit-transform: rotate(60deg) scale(.6);}'+
+          '50%{-webkit-transform: rotate(330deg) scale(.6);}'+
+          'to{-webkit-transform: rotate(59deg) scale(.6);}'+
+        '}'+
+        '@-webkit-keyframes v6{'+
+          'from{-webkit-transform: rotate(-150deg) scale(.5);}'+
+          '50%{-webkit-transform: rotate(-420deg) scale(.5);}'+
+          'to{-webkit-transform: rotate(-149deg) scale(.5);}'+
+        '}'+
+        '@keyframes v1{'+
+          'from{transform: rotate(0deg) scale(1);}'+
+          '50%{transform: rotate(270deg) scale(1);}'+
+          'to{transform: rotate(1deg) scale(1);}'+
+        '}'+
+        '@keyframes v2{'+
+          'from{transform: rotate(-90deg) scale(.9);}'+
+          '50%{transform: rotate(-360deg) scale(.9);}'+
+          'to{transform: rotate(-89deg) scale(.9);}'+
+        '}'+
+        '@keyframes v3{'+
+          'from{transform: rotate(30deg) scale(.8);}'+
+          '50%{transform: rotate(300deg) scale(.8);}'+
+          'to{transform: rotate(29deg) scale(.8);}'+
+        '}'+
+        '@keyframes v4{'+
+          'from{transform: rotate(-120deg) scale(.7);}'+
+          '50%{transform: rotate(-390deg) scale(.7);}'+
+          'to{transform: rotate(-119deg) scale(.7);}'+
+        '}'+
+        '@keyframes v5{'+
+          'from{transform: rotate(60deg) scale(.6);}'+
+          '50%{transform: rotate(330deg) scale(.6);}'+
+          'to{transform: rotate(59deg) scale(.6);}'+
+        '}'+
+        '@keyframes v6{'+
+          'from{transform: rotate(-150deg) scale(.5);}'+
+          '50%{transform: rotate(-420deg) scale(.5);}'+
+          'to{transform: rotate(-149deg) scale(.5);}'+
+        '}';
+      }
+      htmlCode += '</style>';
+
+      q.innerHTML += htmlCode;
       b.appendChild(q);
 
       var h = w.innerHeight;
@@ -114,7 +119,14 @@
       var c = new Array();
       var k = 0;
       d.addEventListener('scroll',function(){u = d.documentElement.scrollTop || b.scrollTop;},false);
-      for(var i=0;i<50;i++){
+      let petalCount;
+      let fallIntervalMsec = 60;
+      if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+        petalCount = 15;
+      } else {
+        petalCount = 50;
+      }
+      for(var i=0;i<petalCount;i++){
         var m = d.createElement('div');
         m.id = 'hanabira'+i;
         t[i] = Math.random()*-1000+u;
@@ -128,8 +140,9 @@
         g[i] = d.getElementById('hanabira'+i);
         c[i] = 0;
       }
-      setInterval(function(){
-        for(var i=0;i<50;i++){
+
+      function fallPetalsToNextPosition(){
+        for(var i=0;i<petalCount;i++){
           if(t[i]<u+h-40){
             if(y[i]>=c[i]){
               l[i] = l[i]+0.5+Math.random()*0.5;
@@ -176,7 +189,9 @@
         }
         k++;
         /* console.log(k); */
-      },45);
+      }
+
+      setInterval(fallPetalsToNextPosition,fallIntervalMsec);
     })(window.document,window.document.body,window);
   });
 })(jQuery);
